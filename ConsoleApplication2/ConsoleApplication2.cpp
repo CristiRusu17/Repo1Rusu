bebbe100127552f75937a0662ad7cc2138d0093e
AndreiSiu <vasiu_andrei2000@yahoo.com>
2020-03-02 11:41:33 +0200

    Update ConsoleApplication2.cpp

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Sal ba!!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
